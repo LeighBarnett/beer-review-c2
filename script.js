c2af26fcdfaf9c6802143376cbc26108593e27e0
LeighBarnett <leighbarnett5@gmail.com>
2017-06-01 16:19:55 +0300

    Finished assignment

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 var books = [];
+var sortedBooks = [];
 
-function addBook(bookName, bookCategory) {
+function addBook(bookName, bookCategory, bookRating) {
     var newBook = new Object();
     newBook.name = bookName;
     newBook.category = bookCategory;
+    newBook.rating = bookRating;
     books.push(newBook);
+    sortedBooks = books.sort(function(a, b) {
+        return b.rating - a.rating });
 }
 $('.post-book').click(function() {
     var bookName = $('.book-input').val();
     var bookCategory = $('.category-input').val();
-    addBook(bookName, bookCategory);
+    var bookRating = $('.rate-input').val();
+    addBook(bookName, bookCategory, bookRating);
     updateBooks();
 });
 
+// function changeToText(bookRating){
+    
+//     switch(bookRating) {
+//     case "1":
+        
+//         break;
+//     case n:
+//         code block
+//         break;
+//     default:
+//         code block
+// }
+// }
+
 function updateBooks() {
     $('.books-list').empty();
-    for (var i = 0; i < books.length; i++) {
-        $('.books-list').append("<li>" + "Title: " + books[i].name + " - Category: " + books[i].category + "</li");
-    };
+    for (var i = 0; i < sortedBooks.length; i++) {
+        $('.books-list').append("<li>" + "Title: " + sortedBooks[i].name + 
+            " Category: " + sortedBooks[i].category + 
+            " Rating: " + sortedBooks[i].rating + "</li>");
+    }
+
 };
+
